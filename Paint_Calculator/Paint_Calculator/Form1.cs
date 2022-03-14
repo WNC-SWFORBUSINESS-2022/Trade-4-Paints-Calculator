b0e395171f977d6e8e1fcf8fa725028efadc90a0
Peppy10003 <67559143+peppy10003@users.noreply.github.com>
2022-03-14 16:22:35 +0000

    Uploaded Crap
    Git being dummy thicc for making me upload it manually...

---
+++ Paint_Calculator/Paint_Calculator/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// holds the visual stuff
+List<string> Input_List = new List<string>(); // use biger version
+// holds the more complex information
+List<string> Input_Type = new List<string>();
+//holds something...
+List<string> Section_Type = new List<string>();
+//exchange rate
+List<string> Exchange_Rate = new List<string>();
+
+// therse are temporary information
+string Section = "";
+List<string> Section_Units;
+int Current_Section_Type = 0;
+int Current_Section_Unit = 0;
+namespace Paint_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int Current_Section_Type != 0)
+            {
+                Section_Type.append(Curent_Section_Type);
+                Section == Section + "1";
+                Section_Type = 1
+            }
+            else
+            {
+            Section == Section + "1";
+            }
+        }
+    }
+}

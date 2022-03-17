9abdc858892873e5866ecafdec68a37d43f2efbe
Peppy10003 <67559143+peppy10003@users.noreply.github.com>
2022-03-17 01:34:58 +0000

    Implemented my code to the actual thing
    Hi Tommy :D
    only got the total to do (also make sure the reset button works properly)

--- Paint_Calculator/Paint_Calculator/Form1.Designer.cs
+++ Paint_Calculator/Paint_Calculator/Form1.Designer.cs
@@ -51,12 +51,13 @@ namespace Paint_Calculator
             this.button18 = new System.Windows.Forms.Button();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(13, 70);
+            this.button1.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.button1.Location = new System.Drawing.Point(12, 60);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 58);
             this.button1.TabIndex = 0;
@@ -66,211 +67,231 @@ namespace Paint_Calculator
             // 
             // textBox1
             // 
-            this.textBox1.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(394, 51);
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(394, 41);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(413, 12);
+            this.textBox2.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.textBox2.Location = new System.Drawing.Point(412, 12);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(94, 51);
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(94, 41);
             this.textBox2.TabIndex = 2;
             // 
             // button2
             // 
-            this.button2.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(113, 70);
+            this.button2.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.button2.Location = new System.Drawing.Point(112, 60);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(94, 58);
             this.button2.TabIndex = 3;
             this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(213, 70);
+            this.button3.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.button3.Location = new System.Drawing.Point(212, 60);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(94, 58);
             this.button3.TabIndex = 4;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(13, 134);
+            this.button4.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.button4.Location = new System.Drawing.Point(12, 124);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(94, 58);
             this.button4.TabIndex = 5;
             this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(113, 134);
+            this.button5.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.button5.Location = new System.Drawing.Point(113, 124);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(94, 58);
             this.button5.TabIndex = 6;
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
-            this.button6.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(213, 134);
+            this.button6.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.button6.Location = new System.Drawing.Point(212, 124);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(94, 58);
             this.button6.TabIndex = 7;
             this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
-            this.button7.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.Location = new System.Drawing.Point(13, 198);
+            this.button7.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.button7.Location = new System.Drawing.Point(12, 188);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(94, 58);
             this.button7.TabIndex = 8;
             this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
-            this.button8.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button8.Location = new System.Drawing.Point(113, 198);
+            this.button8.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.button8.Location = new System.Drawing.Point(112, 188);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(94, 58);
             this.button8.TabIndex = 9;
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
-            this.button9.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button9.Location = new System.Drawing.Point(213, 198);
+            this.button9.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.button9.Location = new System.Drawing.Point(212, 188);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(94, 58);
             this.button9.TabIndex = 10;
             this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
-            this.button10.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button10.Location = new System.Drawing.Point(113, 262);
+            this.button10.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.button10.Location = new System.Drawing.Point(112, 252);
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(94, 58);
             this.button10.TabIndex = 11;
             this.button10.Text = "0";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button11
             // 
-            this.button11.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button11.Location = new System.Drawing.Point(213, 262);
+            this.button11.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.button11.Location = new System.Drawing.Point(212, 252);
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(94, 58);
             this.button11.TabIndex = 12;
-            this.button11.Text = "button11";
+            this.button11.Text = "Unit (mm)";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // button12
             // 
-            this.button12.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button12.Location = new System.Drawing.Point(313, 70);
+            this.button12.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.button12.Location = new System.Drawing.Point(312, 60);
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(94, 58);
             this.button12.TabIndex = 13;
-            this.button12.Text = "button12";
+            this.button12.Text = "+";
             this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
             // 
             // button13
             // 
-            this.button13.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button13.Location = new System.Drawing.Point(313, 134);
+            this.button13.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.button13.Location = new System.Drawing.Point(312, 124);
             this.button13.Name = "button13";
             this.button13.Size = new System.Drawing.Size(94, 58);
             this.button13.TabIndex = 14;
-            this.button13.Text = "button13";
+            this.button13.Text = "-";
             this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
             // 
             // button14
             // 
-            this.button14.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button14.Location = new System.Drawing.Point(313, 198);
+            this.button14.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.button14.Location = new System.Drawing.Point(312, 188);
             this.button14.Name = "button14";
             this.button14.Size = new System.Drawing.Size(94, 58);
             this.button14.TabIndex = 15;
-            this.button14.Text = "button14";
+            this.button14.Text = "X";
             this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
             // 
             // button15
             // 
-            this.button15.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button15.Location = new System.Drawing.Point(313, 262);
+            this.button15.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.button15.Location = new System.Drawing.Point(312, 252);
             this.button15.Name = "button15";
             this.button15.Size = new System.Drawing.Size(94, 58);
             this.button15.TabIndex = 16;
-            this.button15.Text = "button15";
+            this.button15.Text = "/";
             this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
             // 
             // button16
             // 
-            this.button16.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button16.Location = new System.Drawing.Point(13, 262);
+            this.button16.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.button16.Location = new System.Drawing.Point(12, 252);
             this.button16.Name = "button16";
             this.button16.Size = new System.Drawing.Size(94, 58);
             this.button16.TabIndex = 17;
-            this.button16.Text = "button16";
+            this.button16.Text = ".";
             this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
             // 
             // button17
             // 
-            this.button17.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button17.Location = new System.Drawing.Point(413, 70);
+            this.button17.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.button17.Location = new System.Drawing.Point(412, 60);
             this.button17.Name = "button17";
-            this.button17.Size = new System.Drawing.Size(94, 122);
+            this.button17.Size = new System.Drawing.Size(94, 58);
             this.button17.TabIndex = 18;
-            this.button17.Text = "button17";
+            this.button17.Text = "Reset";
             this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
             // 
             // button18
             // 
-            this.button18.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button18.Location = new System.Drawing.Point(413, 198);
+            this.button18.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.button18.Location = new System.Drawing.Point(412, 124);
             this.button18.Name = "button18";
-            this.button18.Size = new System.Drawing.Size(94, 122);
+            this.button18.Size = new System.Drawing.Size(94, 186);
             this.button18.TabIndex = 19;
-            this.button18.Text = "button18";
+            this.button18.Text = "Enter";
             this.button18.UseVisualStyleBackColor = true;
             // 
             // textBox3
             // 
-            this.textBox3.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(13, 326);
+            this.textBox3.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.textBox3.Location = new System.Drawing.Point(12, 316);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(394, 51);
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(394, 41);
             this.textBox3.TabIndex = 20;
             // 
             // textBox4
             // 
-            this.textBox4.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(413, 326);
+            this.textBox4.Font = new System.Drawing.Font("Javanese Text", 11F);
+            this.textBox4.Location = new System.Drawing.Point(412, 316);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(94, 51);
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(94, 41);
             this.textBox4.TabIndex = 21;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(519, 392);
+            this.ClientSize = new System.Drawing.Size(519, 370);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button18);
@@ -324,6 +345,7 @@ namespace Paint_Calculator
         private System.Windows.Forms.Button button18;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- Paint_Calculator/Paint_Calculator/Form1.cs
+++ Paint_Calculator/Paint_Calculator/Form1.cs
@@ -8,41 +8,250 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-// holds the visual stuff
-List<string> Input_List = new List<string>(); // use biger version
-// holds the more complex information
-List<string> Input_Type = new List<string>();
-//holds something...
-List<string> Section_Type = new List<string>();
-//exchange rate
-List<string> Exchange_Rate = new List<string>();
 
-// therse are temporary information
-string Section = "";
-List<string> Section_Units;
-int Current_Section_Type = 0;
-int Current_Section_Unit = 0;
 namespace Paint_Calculator
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        List<int> Input_Type = new List<int>(); // Holds the more complex information: numbers = 0, method = 1, Unit = 2; can then be used for visual translation
 
-        private void button1_Click(object sender, EventArgs e)
+        List<Double> Input_Numbers = new List<Double>(); // Holds the Numeric values
+        List<int> Input_Units = new List<int>(); // Holds more specific information for Units (0 - 3)
+        List<Double> Input_Operators = new List<Double>(); // Holds more specific information for Methods(0 - 3)
+
+        Double Section = 0; // Numbers and Operators fight for this variable
+
+        // ram
+        int Current_Section_Type = 0;// Numbers = 0, Operators = 1
+        int Current_Section_Unit = 0; // (0 - 3)
+
+        //Library
+        string[] Unit_Values = { "mm", "cm", "m", "km" };
+        string[] L_Operators = { "+", "-", "*", "/" };
+        int[] Exchange_Rate = { 1000, 100, 1, 1000 };
+
+        int Cycle = 0; // Cycles what Unit the user wants
+        bool Decimal_Enable = false;
+
+        #region Functions
+        public void Number(Double x)
         {
-            if (int Current_Section_Type != 0)
+            if (Current_Section_Type == 1) // Doesn't like Numbers
             {
-                Section_Type.append(Curent_Section_Type);
-                Section == Section + "1";
-                Section_Type = 1
+                Input_Type.Add(1); // Adds Operator to type list be fore replacing it with Number type
+                Input_Operators.Add(Section); // Adds the Operator to the Operator list
+                
+                Current_Section_Type = 0;
+                Section = x;
+
+                textBox1_Update(); // Run update for text box
+            }
+            else if (Decimal_Enable == true)
+            {
+                Section = Double.Parse(Section.ToString() + '.' + x.ToString());
+                textBox2.Text = Section.ToString();
             }
             else
             {
-            Section == Section + "1";
+                Section = Double.Parse(Section.ToString() + x.ToString()); // inputs number;
+                textBox2.Text = Section.ToString();
             }
+        }
+
+        public void Operation(int x)
+        {
+            if (Current_Section_Type == 0) // Doesn't like Operators
+            {
+                Input_Type.Add(0); // add numbers to type list be fore replacing it with Operator type
+                Input_Numbers.Add(Section); // add numbers to the visual list
+                
+                Input_Type.Add(2); // Adds the Unit type to the list
+                Input_Units.Add(Current_Section_Unit); // adds the specific unit to the list
+
+                Current_Section_Type = 1; // Finally does the Operator
+                Section = (x);
+                textBox2.Text = L_Operators[Convert.ToInt32(Section)];
+
+                textBox1_Update();
+            }
+            else
+            {
+                Section = (x);
+                textBox2.Text = L_Operators[Convert.ToInt32(Section)];
+            }
+        }
+
+        public void Unit(int x)
+        {
+            Current_Section_Unit = x;
+            button11.Text = Unit_Values[x];
+        }
+
+        public void textBox1_Update()
+        {
+            int Length = Input_Type.Count(); // Calculate the length of the list
+
+            string Display = "";
+
+            int Number_Count = 0;
+            int Operator_Count = 0;
+            int Unit_Count = 0;
+
+            for (int i = 0; i < Length; i++)
+            {
+                switch (Input_Type[i])
+                {
+                    case 0: // Numerals
+                        Display = Display + Input_Numbers[Number_Count];
+                        Number_Count = Number_Count + 1;
+                        break;
+
+                    case 1: // Operators
+                        Display = Display + L_Operators[Convert.ToInt32(Input_Operators[Operator_Count])];
+                        Operator_Count = Operator_Count + 1;
+                        break;
+
+                    case 2: // Units
+                        Display = Display + Unit_Values[Convert.ToInt32(Input_Units[Unit_Count])];
+                        Unit_Count = Unit_Count + 1;
+                        break;
+
+                    default:
+                        // Nothing
+                        break;
+                }
+            }
+            textBox1.Text = Display;
+        }
+
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+        } //ignore for now
+        
+        // Holds the main numerical inputs (Includes Decimal)
+        #region Number Input
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Number(1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Number(2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Number(3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Number(4);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Number(5);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Number(6);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Number(7);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Number(8);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Number(9);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Number(0);
+        }
+        
+        // Decimal
+        private void button16_Click(object sender, EventArgs e)
+        {
+            if (Current_Section_Type == 0)
+            {
+                if (Decimal_Enable == true)
+                    {
+                        Decimal_Enable = false;
+                    }
+                else
+                    {
+                        Decimal_Enable = true;
+                    }
+            }
+        }
+        #endregion
+
+        // Change Unit
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (Cycle == 3)
+            {
+                Cycle = 0;
+            }
+            else
+            {
+                Cycle = Cycle + 1;
+            }
+            Unit(Cycle);
+        }
+        
+        // Operators, Duh... (holds the inputs for operations like additon or division)
+        #region Operators
+        // Plus
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Operation(0);
+        }
+
+        // Minus
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Operation(1);
+        }
+
+        // Times
+        private void button14_Click(object sender, EventArgs e)
+        {
+            Operation(2);
+        }
+
+        // Division
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Operation(3);
+        }
+
+        #endregion
+
+        // Reset (resets averything before User Input (Ignoring Unit))
+        private void button17_Click(object sender, EventArgs e)
+        {
+            Input_Type.Clear();
+
+            Input_Numbers.Clear();
+            Input_Units.Clear();
+            Input_Operators.Clear();
+
+            Section = 0;
+            Current_Section_Type = 0;
         }
     }
 }
